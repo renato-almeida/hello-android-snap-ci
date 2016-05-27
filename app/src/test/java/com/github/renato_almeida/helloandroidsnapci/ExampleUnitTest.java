faffe8a4672957e9b35fe0362e9ba043c2c20ff2
Renato Almeida <renato.m.alm@gmail.com>
2016-05-27 15:42:00 +0100

    Updated unit tests

--- app/src/test/java/com/github/renato_almeida/helloandroidsnapci/ExampleUnitTest.java
+++ app/src/test/java/com/github/renato_almeida/helloandroidsnapci/ExampleUnitTest.java
@@ -2,11 +2,14 @@ package com.github.renato_almeida.helloandroidsnapci;
 
 import org.junit.Test;
 
+import android.test.suitebuilder.annotation.SmallTest;
+
 import static org.junit.Assert.*;
 
 /**
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
  */
+@SmallTest
 public class ExampleUnitTest {
 
     @Test
