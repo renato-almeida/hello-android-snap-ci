c3aca0e6beeef3d87496f8a64416c931928e80bf
Renato Almeida <renato.m.alm@gmail.com>
2016-05-27 16:24:55 +0100

    Added android instrumentation test

--- app/src/androidTest/java/com/github/renato_almeida/helloandroidsnapci/ApplicationTest.java
+++ app/src/androidTest/java/com/github/renato_almeida/helloandroidsnapci/ApplicationTest.java
@@ -8,6 +8,14 @@ import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.allOf;
+
 @RunWith(AndroidJUnit4.class)
 @LargeTest
 public class ApplicationTest {
@@ -18,6 +26,9 @@ public class ApplicationTest {
 
     @Test
     public void buttonClickTest() {
+        onView(withId(R.id.fab)).perform(click());
 
+        onView(allOf(withId(android.support.design.R.id.snackbar_text),
+                withText(R.string.fab_click_text))).check(matches(isDisplayed()));
     }
 }
